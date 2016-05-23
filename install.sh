50b532f9638253a09c6f033f1aaa0f76cdbb22f5
Wangzongcan <wang.zongcan@gmail.com>
2016-05-23 15:43:55 +0800

    创建文件夹 参数

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir ~/.vim/tmp/undo
-mkdir ~/.vim/tmp/backup
-mkdir ~/.vim/tmp/swap
+mkdir -p ~/.vim/tmp/undo
+mkdir -p ~/.vim/tmp/backup
+mkdir -p ~/.vim/tmp/swap
